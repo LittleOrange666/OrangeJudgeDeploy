54abc342636091f05885ffbe90b458c7dcd80fe9
LittleOrange666 <orangeminecraft123@gmail.com>
2025-06-24 01:13:44 +0800

    support cloudflare

--- install.py
+++ install.py
@@ -4,6 +4,51 @@ import shutil
 import urllib.request
 
 import yaml
+from scipy.signal import wiener
+
+
+def security_tools():
+    while True:
+        print("Choose a operation:")
+        print("1. Go back")
+        print("2. Remove exposed ports")
+        print("3. Add cloudflare proxy (need tunnel token)")
+        choice = input("Enter your choice: ")
+        with open("docker-compose.yml", encoding="utf8") as f:
+            info = yaml.load(f, Loader=yaml.FullLoader)
+        if choice == "1":
+            pass
+        elif choice == "2":
+            if not os.path.exists("docker-compose.yml"):
+                print("docker-compose.yml not found.")
+                break
+            for service in info["services"]:
+                if "judge_backend" != service and "ports" in info["services"][service]:
+                    del info["services"][service]["ports"]
+            print("Removed exposed ports from all services except judge_backend")
+        elif choice == "3":
+            token = input("Enter your cloudflare tunnel token: ")
+            template = {
+                "image": "cloudflare/cloudflared:latest",
+                "restart": "unless-stopped",
+                "command": "tunnel --url http://judge_backend:8080 --no-autoupdate run",
+                "environment": [
+                    f"TUNNEL_TOKEN={token}"
+                ],
+                "depends_on": {
+                    "judge_backend": {
+                        "condition": "service_healthy"
+                    }
+                }
+            }
+            info["services"]["cloudflare"] = template
+            print("Added cloudflare proxy service to docker-compose.yml")
+        else:
+            print("Invalid choice, please try again")
+            continue
+        with open("docker-compose.yml", "w", encoding="utf8") as f:
+            yaml.dump(info, f)
+        break
 
 
 def main():
@@ -31,14 +76,14 @@ def main():
             l.append(k + "=" + v)
 
         judge_token = secrets.token_urlsafe(33)
-        upd("judge_server","JUDGE_TOKEN", judge_token)
-        upd("judge_backend","JUDGE_TOKEN", judge_token)
+        upd("judge_server", "JUDGE_TOKEN", judge_token)
+        upd("judge_backend", "JUDGE_TOKEN", judge_token)
         db_password = secrets.token_urlsafe(21)
-        upd("judge_mariadb","MYSQL_PASSWORD", db_password)
-        upd("judge_mariadb","MYSQL_ROOT_PASSWORD", secrets.token_urlsafe(21))
-        upd("judge_backend","MYSQL_PASSWORD", db_password)
+        upd("judge_mariadb", "MYSQL_PASSWORD", db_password)
+        upd("judge_mariadb", "MYSQL_ROOT_PASSWORD", secrets.token_urlsafe(21))
+        upd("judge_backend", "MYSQL_PASSWORD", db_password)
         flask_secret_key = secrets.token_urlsafe(21)
-        upd("judge_backend","FLASK_SECRET_KEY", flask_secret_key)
+        upd("judge_backend", "FLASK_SECRET_KEY", flask_secret_key)
         with open("docker-compose.yml", "w", encoding="utf8") as f:
             yaml.dump(info, f)
         print("Download docker-compose.yml successfully")
@@ -56,16 +101,19 @@ def main():
         print("Choose a operation:")
         print("1. Quit")
         print("2. start judge")
+        print("3. security tools")
         for i, lang in enumerate(langs):
-            print(f"{i + 3}. install {lang}")
+            print(f"{i + 4}. install {lang}")
         choice = input("Enter your choice: ")
         if choice == "1":
             break
         elif choice == "2":
             os.system("docker-compose up -d")
             print("Judge server started")
-        elif choice.isdigit() and 3 <= int(choice) <= len(langs) + 2:
-            lang = langs[int(choice) - 3]
+        elif choice == "3":
+            security_tools()
+        elif choice.isdigit() and 4 <= int(choice) <= len(langs) + 3:
+            lang = langs[int(choice) - 4]
             os.system(f"python3 OrangeJudgeLangs/{lang}.py")
         else:
             print("Invalid choice, please try again")
